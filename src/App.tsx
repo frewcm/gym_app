1f3560fa6eddbe07e268a6601a5c5f4610216776
frewcm <frewbefekadu32@gmail.com>
2023-04-07 13:51:04 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import NavBar from "@/components/navbar/NavBar";
+
+function App() {
+  return (
+    <div className="bg-gray-20">
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navbar/NavBar.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import Logo from "@/assets/Logo.png";
+
+type Props = {};
+
+const NavBar = (props: Props) => {
+  const flexBetween = "flex items-center justify-between";
+  return (
+    <nav>
+      <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+        <div className={`${flexBetween} mx-auto w-5/6`}>
+          <div className={`${flexBetween} w-full gap-16`}>
+            {/* right-side */}
+            <img src={Logo} alt="logo" />
+            {/* left-side */}
+            <div className={`${flexBetween} w-full`}>
+              <div className={`${flexBetween} gap-8 text-sm`}>
+                <p>Home</p>
+                <p>Benefits</p>
+                <p>Our Classes</p>
+                <p>Contact Us</p>
+              </div>
+              <div className={`${flexBetween} gap-8`}>
+                <p>Sign In</p>
+                <button>Become A Member</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
