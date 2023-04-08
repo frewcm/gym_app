e25df21f63bef846b9291296752517993a53fa0a
frewcm <frewbefekadu32@gmail.com>
2023-04-08 11:02:13 +0300

    navbar finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
 import NavBar from "@/components/navbar/NavBar";
+import { useEffect, useState } from "react";
+import { SelectedPage } from "@/shared/types";
 
 function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      } else {
+        setIsTopOfPage(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  });
   return (
     <div className="bg-gray-20">
-      <NavBar />
+      <NavBar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
     </div>
   );
 }
